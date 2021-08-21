59fd5ea63a9b74894bb346859869c9bc7954ed8e
jesus re <eisa.eisa.r.r@gmail.com>
2021-08-21 15:23:51 +0430

    deploy problem

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_SERVER,
+} = require("next/constants");
+
+module.exports = (phase) => {
+  let isClient = "1";
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_SERVER) {
+    isClient = "0";
+  }
+
+  /* config options for all phases except development here */
+  return {
+    target: "serverless",
+    env: {
+      isClient,
+    },
+  };
+};
