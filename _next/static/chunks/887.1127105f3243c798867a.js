7e58d23f0b9f346faa77549f6d37b593c5a37ef5
eisa-rezaei <78264204+eisa-rezaei@users.noreply.github.com>
2021-08-22 20:10:55 +0000

    Deploying to gh-pages from  @ f6fe348960652d26dc6ea3d9c10616d1dc16b89f 🚀

--- _next/static/chunks/887.1127105f3243c798867a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[887],{5887:function(e,n,u){u.r(n);var r=u(5893);n.default=function(){return(0,r.jsx)("h1",{children:"Pricing page in under development..."})}}}]);
